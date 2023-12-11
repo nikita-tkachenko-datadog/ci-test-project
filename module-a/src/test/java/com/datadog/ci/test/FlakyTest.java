5143e4741619fc97ef35732f7cf480e21400c7ba
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-12-11 11:55:57 +0100

    Add a flaky test

---
+++ module-a/src/test/java/com/datadog/ci/test/FlakyTest.java
@@ -0,0 +1,14 @@
+package com.datadog.ci.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.Test;
+
+public class FlakyTest {
+
+    @Test
+    public void testFlaky() {
+        assertEquals(0, ThreadLocalRandom.current().nextInt(4));
+    }
+}
