87ccf61d7056d800ba11dc9969a91e3bc0965fa2
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2023-12-11 12:44:03 +0100

    Migrate everything to JUnit 5

--- module-a/src/test/java/com/datadog/ci/test/CalculatorTest.java
+++ module-a/src/test/java/com/datadog/ci/test/CalculatorTest.java
@@ -1,60 +1,34 @@
 package com.datadog.ci.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TestRule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-    @ClassRule
-    public static final TestRule CLASS_RULE = (base, description) -> {
-        System.out.println("Applying class rule " + CalculatorTest.class);
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return base;
-    };
-
-    @Rule
-    public final TestRule testCaseRule = (base, description) -> {
-        System.out.println("Applying test case rule " + this);
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return base;
-    };
-
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() throws InterruptedException {
         System.out.println("Setting up " + CalculatorTest.class);
         Thread.sleep(50);
     }
 
-    @Before
+    @BeforeEach
     public void setUp() throws InterruptedException {
         System.out.println("Setting up test case " + this);
         Thread.sleep(50);
     }
 
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() throws InterruptedException {
         System.out.println("Tearing down " + CalculatorTest.class);
         Thread.sleep(50);
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws InterruptedException {
         System.out.println("Tearing down test case " + this);
         Thread.sleep(50);
@@ -66,7 +40,6 @@ public class CalculatorTest {
         assertEquals(4, c.add(2, 2));
     }
 
-    @Ignore
     @Test
     public void testSubtraction() {
         Calculator c = new Calculator();
--- module-a/src/test/java/com/datadog/ci/test/FlakyTest.java
+++ module-a/src/test/java/com/datadog/ci/test/FlakyTest.java
@@ -1,9 +1,9 @@
 package com.datadog.ci.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.concurrent.ThreadLocalRandom;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class FlakyTest {
 
